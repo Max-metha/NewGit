16e8a6cda8654ab8370b8eeb4137078c8f4277ef
Max Hartwig <max.hartwig@ymail.com>
2022-07-15 13:27:32 +0200

    greeter-twice

--- greeter.java
+++ greeter.java
@@ -4,5 +4,6 @@ class Greeter {
     this.name = name;
     }
     public String greet(){
-    return "Hello " + name + ", what a wonderful day."
+    return "Hello " + name + ", what a wonderful day.";
     }
+}
